3dde5b2c94fd369b3c2f1359218aeb126e9eea23
chosangwoo <kajer102@gmail.com>
2022-11-15 16:19:24 +0900

    Initial Commit

---
+++ StockRank/StockRankCollectionViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  StockRankCollectionViewCell.swift
+//  StockRank
+//
+//  Created by 조상우 on 2022/10/24.
+//
+
+import UIKit
+
+class StockRankCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var rankLabel: UILabel!
+    
+    @IBOutlet weak var companyIconImageView: UIImageView!
+    
+    @IBOutlet weak var companyNameLabel: UILabel!
+    
+    @IBOutlet weak var companyPriceLabel: UILabel!
+    
+    @IBOutlet weak var diffLable: UILabel!
+    
+    @IBOutlet weak var diffLabel: UILabel!
+    
+    func configure(_ stock: StockModel) {
+        rankLabel.text = "\(stock.rank)"
+        companyIconImageView.image = UIImage(named: stock.imageName)
+        companyNameLabel.text = stock.name
+        companyPriceLabel.text = "\(convertToCurrencyFormat(price: stock.price)) 원"
+        
+//        let color: UIColor
+//        if stock.diff > 0 {
+//            color = UIColor.systemRed
+//        } else {
+//            color = UIColor.systemBlue
+//        }
+        diffLabel.textColor = stock.diff > 0 ? UIColor.systemRed : UIColor.systemBlue
+        diffLabel.text = "\(stock.diff)%" 
+    }
+    
+    func convertToCurrencyFormat(price: Int) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = 0
+        let result = numberFormatter.string(from: NSNumber(value: price)) ?? ""
+        return result
+    }
+}
